b6304c3a7b82d6af9c0b2527cb67c05340f589f1
Maciej Jablonski <m.jablonski@oberthur.com>
2016-04-13 13:04:21 +0200

    Added port specification for docker entrypoint

--- README.md
+++ README.md
@@ -5,6 +5,12 @@ You need edit (add) this env:
 - **STORE_USAGE**: value in GB (default value is 10)
 - **TEMP_USAGE**: value in GB (default value is 5)
 - **ADMIN_PASSWORD**: provide admin password (default admin123)
+- **PORT_OPENWIRE**: port for openwire connection (default value is 61616)
+- **PORT_AMQP**: port for AMQP connection (default value is 5672)
+- **PORT_STOMPSSL**: port for stomp/SSL connection (default value is 61612)
+- **PORT_STOMP**: port for stomp connection (default value is 61613)
+- **PORT_MQTT**: port for MQTT connection (default value is 1883)
+- **PORT_WS**: port for WebSocket connection (default value is 61614)
 
 If you want web console you should expose:
 - **8161**: if you need plain http connection
--- start-activemq.sh
+++ start-activemq.sh
@@ -5,9 +5,21 @@ set -x
 STORE_USAGE=${STORE_USAGE:=10}
 TEMP_USAGE=${TEMP_USAGE:=5}
 ADMIN_PASSWORD=${ADMIN_PASSWORD:=admin123}
+PORT_OPENWIRE=${PORT_OPENWIRE:=61616}
+PORT_AMQP=${PORT_AMQP:=5672}
+PORT_STOMPSSL=${PORT_STOMPSSL:=61612}
+PORT_STOMP=${PORT_STOMP:=61613}
+PORT_MQTT=${PORT_MQTT:=1883}
+PORT_WS=${PORT_WS:=61614}
 
 sed -i -e "s/<storeUsage limit=\"100 gb\"\/>/<storeUsage limit=\"${STORE_USAGE} gb\"\/>/" /opt/app/apache-activemq/conf/activemq.xml
 sed -i -e "s/<tempUsage limit=\"50 gb\"\/>/<tempUsage limit=\"5 gb\"\/>/" /opt/app/apache-activemq/conf/activemq.xml
+sed -i -e "s/<transportConnector name=\"openwire\" uri=\"tcp:\/\/0.0.0.0:61616?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"openwire\" uri=\"tcp:\/\/0.0.0.0:${PORT_OPENWIRE}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>\//g" /opt/app/apache-activemq/conf/activemq.xml
+sed -i -e "s/<transportConnector name=\"amqp\" uri=\"amqp:\/\/0.0.0.0:5672?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"amqp\" uri=\"amqp:\/\/0.0.0.0:${PORT_AMQP}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>\//g" /opt/app/apache-activemq/conf/activemq.xml
+sed -i -e "s/<transportConnector name=\"stompssl\" uri=\"stomp+nio+ssl:\/\/0.0.0.0:61612?transport.enabledCipherSuites=SSL_RSA_WITH_RC4_128_SHA,SSL_DH_anon_WITH_3DES_EDE_CBC_SHA\" \/>/<transportConnector name=\"stompssl\" uri=\"stomp+nio+ssl:\/\/0.0.0.0:${PORT_STOMPSSL}\?transport.enabledCipherSuites=SSL_RSA_WITH_RC4_128_SHA,SSL_DH_anon_WITH_3DES_EDE_CBC_SHA\" \/>\//g" /opt/app/apache-activemq/conf/activemq.xml
+sed -i -e "s/<transportConnector name=\"stomp\" uri=\"stomp:\/\/0.0.0.0:61613?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"stomp\" uri=\"stomp:\/\/0.0.0.0:${PORT_STOMP}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/g" /opt/app/apache-activemq/conf/activemq.xml
+sed -i -e "s/<transportConnector name=\"mqtt\" uri=\"mqtt:\/\/0.0.0.0:1883?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"mqtt\" uri=\"mqtt:\/\/0.0.0.0:${PORT_MQTT}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/g" /opt/app/apache-activemq/conf/activemq.xml
+sed -i -e "s/<transportConnector name=\"ws\" uri=\"ws:\/\/0.0.0.0:61614?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"ws\" uri=\"ws:\/\/0.0.0.0:${PORT_WS}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/g" /opt/app/apache-activemq/conf/activemq.xml
 
 sed -i -e "s/admin activemq/admin ${ADMIN_PASSWORD}/" /opt/app/apache-activemq/conf/jmx.password
 sed -i -e "s/admin: admin, admin/admin: ${ADMIN_PASSWORD}, admin/" /opt/app/apache-activemq/conf/jetty-realm.properties
@@ -16,3 +28,4 @@ sed -i -e 's/user: user, user//' /opt/app/apache-activemq/conf/jetty-realm.prope
 JVM_OPTS="-server -verbose:gc -XX:+UseCompressedOops -Xms512m -Xmx512m -XX:MetaspaceSize=64M -XX:MaxMetaspaceSize=64M"
 
 exec java ${JVM_OPTS} -Djava.util.logging.config.file=logging.properties -jar /opt/app/apache-activemq/bin/activemq.jar start
+
