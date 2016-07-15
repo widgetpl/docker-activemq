2550af93b1ea72bbb71fc6978ed5e0507c08fd6d
Mike Dziedziela <michal.dziedziela@gmail.com>
2016-07-15 11:51:59 +0200

    added possibility to configure logging by envioronment variables

--- start-activemq.sh
+++ start-activemq.sh
@@ -11,6 +11,12 @@ PORT_STOMPSSL=${PORT_STOMPSSL:=61612}
 PORT_STOMP=${PORT_STOMP:=61613}
 PORT_MQTT=${PORT_MQTT:=1883}
 PORT_WS=${PORT_WS:=61614}
+ROOT_LOGGER_LEVEL=${ROOT_LOGGER_LEVEL:="INFO, console, logfile"}
+ACTIVEMQ_SPRING_LOGGER_LEVEL=${ACTIVEMQ_SPRING_LOGGER_LEVEL:="WARN"}
+ACTIVEMQ_WEB_HANDLER_LOGGER_LEVEL=${ACTIVEMQ_WEB_HANDLER_LOGGER_LEVEL:="WARN"}
+SPRINGFRAMEWORK_LOGGER_LEVEL=${SPRINGFRAMEWORK_LOGGER_LEVEL:="WARN"}
+CAMEL_LOGGER_LEVEL=${CAMEL_LOGGER_LEVEL:="INFO"}
+CONSOLE_APPENDER_THRESHOLD_LEVEL=${CONSOLE_APPENDER_THRESHOLD_LEVEL:="INFO"}
 
 sed -i -e "s/<storeUsage limit=\"100 gb\"\/>/<storeUsage limit=\"${STORE_USAGE} gb\"\/>/" /opt/app/apache-activemq/conf/activemq.xml
 sed -i -e "s/<tempUsage limit=\"50 gb\"\/>/<tempUsage limit=\"5 gb\"\/>/" /opt/app/apache-activemq/conf/activemq.xml
@@ -21,6 +27,25 @@ sed -i -e "s/<transportConnector name=\"stomp\" uri=\"stomp:\/\/0.0.0.0:61613?ma
 sed -i -e "s/<transportConnector name=\"mqtt\" uri=\"mqtt:\/\/0.0.0.0:1883?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"mqtt\" uri=\"mqtt:\/\/0.0.0.0:${PORT_MQTT}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/g" /opt/app/apache-activemq/conf/activemq.xml
 sed -i -e "s/<transportConnector name=\"ws\" uri=\"ws:\/\/0.0.0.0:61614?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/<transportConnector name=\"ws\" uri=\"ws:\/\/0.0.0.0:${PORT_WS}\?maximumConnections=1000\&amp;wireFormat.maxFrameSize=104857600\"\/>/g" /opt/app/apache-activemq/conf/activemq.xml
 
+# Setting up rootLogger level
+sed -i -e "s/log4j.rootLogger=INFO, console, logfile/log4j.rootLogger=${ROOT_LOGGER_LEVEL}/g" /opt/app/apache-activemq/conf/log4j.properties
+
+# Setting up activemq logger level
+sed -i -e "s/log4j.logger.org.apache.activemq.spring=WARN/log4j.logger.org.apache.activemq=${ACTIVEMQ_SPRING_LOGGER_LEVEL}/g" /opt/app/apache-activemq/conf/log4j.properties
+
+# Setting up activemq web handler logger level
+sed -i -e "s/log4j.logger.org.apache.activemq.web.handler=WARN/log4j.logger.org.apache.activemq.web.handler=${ACTIVEMQ_WEB_HANDLER_LOGGER_LEVEL}/g" /opt/app/apache-activemq/conf/log4j.properties
+
+# Setting up springframework logger level
+sed -i -e "s/log4j.logger.org.springframework=WARN/log4j.logger.org.springframework=${SPRINGFRAMEWORK_LOGGER_LEVEL}/g" /opt/app/apache-activemq/conf/log4j.properties
+
+# Setting up camel logger level
+sed -i -e "s/log4j.logger.org.apache.camel=INFO/log4j.logger.org.apache.camel=${CAMEL_LOGGER_LEVEL}/g" /opt/app/apache-activemq/conf/log4j.properties
+
+# Setting up console appender threshold
+sed -i -e "s/log4j.appender.console.threshold=INFO/log4j.appender.console.threshold=${CONSOLE_APPENDER_THRESHOLD_LEVEL}/g" /opt/app/apache-activemq/conf/log4j.properties
+
+# Enabling Network Of Brokers
 if [[ -v NETWORK_OF_BROKERS_CONNECTORS_URI ]]; then
   sed -i -e "s/<\/shutdownHooks>/<\/shutdownHooks>\\n<networkConnectors>\\n<networkConnector uri=\"${NETWORK_OF_BROKERS_CONNECTORS_URI}\"\/>\\n<\/networkConnectors>/g" /opt/app/apache-activemq/conf/activemq.xml
 fi
